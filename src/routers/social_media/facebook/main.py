208e8df8e7699dc14e4f9e3372a8d2ebbec3314b
KrishnaGupta4135 <guptakrishna4135@gmail.com>
2025-12-16 22:44:10 +0530

    fixed the facebook-login-profile-url added-remove-raw-column

--- src/routers/social_media/facebook/main.py
+++ src/routers/social_media/facebook/main.py
@@ -426,14 +426,14 @@ async def callback(
         existing.name = me_json.get("name")
         existing.email = me_json.get("email")
         existing.picture_url = me_json.get("picture", {})
-        existing.raw_data = me_json
+        # existing.raw_data = me_json
     else:
         new_user = FacebookUser(
         fb_user_id=fb_id,
         name=me_json.get("name"),
         email=me_json.get("email"),
         picture_url=me_json.get("picture", {}),
-        raw_data=me_json,
+        # raw_data=me_json,
         )
         db.add(new_user)
 
